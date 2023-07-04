199ccdfd4d2d8abe89eacf456bcfb010208d8eb3
凉风有信 <3042146237@qq.com>
2023-07-04 22:51:04 +0800

    初始化项目

---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // 箭头函数只有一个参数的时候可以忽略括号
+  arrowParens: 'avoid',
+  // 括号内不出现空格
+  bracketSpacing: true,
+  // 行结束符号用 unix
+  endOfLine: 'lf',
+  jsxBracketSameLine: false,
+  printWidth: 100,
+  // 换行方式
+  proseWrap: 'preserve',
+  // 分号
+  semi: false,
+  // 单引号
+  singleQuote: true,
+  // 缩进
+  tabWidth: 2,
+  // 使用 tabs 缩进
+  useTabs: false,
+  trailingComma: 'es5',
+  parser: 'typescript'
+}
