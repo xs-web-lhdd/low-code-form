e16215217e4bc9ac99c82452944823c326d2da71
凉风有信 <3042146237@qq.com>
2023-07-07 21:43:51 +0800

    refactor: 重构questionCard样式

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,15 @@
 import React, { FC } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
 import Styles from './QuestionCard.module.scss'
+import { Button, Space, Divider, Tag } from 'antd'
+import {
+  EditOutlined,
+  LineChartOutlined,
+  StarOutlined,
+  StarFilled,
+  CopyOutlined,
+  DeleteOutlined,
+} from '@ant-design/icons'
 
 type PropsType = {
   _id: string
@@ -11,28 +21,73 @@ type PropsType = {
 }
 
 const QuestionCard: FC<PropsType> = (props: PropsType) => {
-  const { title, createdAt, answerCount, isPublished } = props
+  const { _id, title, createdAt, answerCount, isPublished, isStar } = props
+  const nav = useNavigate()
+
   return (
     <div className={Styles.container}>
       <div className={Styles.title}>
         <div className={Styles.left}>
-          <a href="#">{title}</a>
+          <Link to={isPublished ? `/question/stat/${_id}` : `/question/edit/${_id}`}>
+            <Space>
+              {isStar && <StarOutlined style={{ color: 'red' }} />}
+              {title}
+            </Space>
+          </Link>
         </div>
         <div className={Styles.right}>
-          {isPublished ? <span style={{ color: 'yellowgreen' }}>已发布</span> : <span>未发布</span>}
-          <span>答卷：{answerCount}</span>
-          <span>{createdAt}</span>
+          <Space>
+            {isPublished ? <Tag color="processing">已发布</Tag> : <Tag>未发布</Tag>}
+            <span>答卷：{answerCount}</span>
+            <span>{createdAt}</span>
+          </Space>
         </div>
       </div>
+      <Divider style={{ margin: '12px' }} />
       <div className={Styles['button-container']}>
         <div className={Styles.left}>
-          <button>编辑问卷</button>
-          <button>数据统计</button>
+          <Space>
+            <Button
+              type="text"
+              size="small"
+              icon={<EditOutlined />}
+              onClick={() => nav(`/question/edit/${_id}`)}
+            >
+              编辑问卷
+            </Button>
+          </Space>
+          <Space>
+            <Button
+              type="text"
+              size="small"
+              icon={<LineChartOutlined />}
+              onClick={() => nav(`/question/stat/${_id}`)}
+              disabled={!isPublished}
+            >
+              数据统计
+            </Button>
+          </Space>
         </div>
         <div className={Styles.right}>
-          <button>标星</button>
-          <button>复制</button>
-          <button>删除</button>
+          <Space>
+            <Button
+              type="text"
+              size="small"
+              icon={isStar ? <StarOutlined /> : <StarFilled style={{ color: 'red' }} />}
+            >
+              {isStar ? '取消标星' : '标星'}
+            </Button>
+          </Space>
+          <Space>
+            <Button type="text" size="small" icon={<CopyOutlined />}>
+              复制
+            </Button>
+          </Space>
+          <Space>
+            <Button type="text" size="small" icon={<DeleteOutlined />}>
+              删除
+            </Button>
+          </Space>
         </div>
       </div>
     </div>
--- src/pages/manage/List/index.tsx
+++ src/pages/manage/List/index.tsx
@@ -17,7 +17,7 @@ const rawQuestionList = [
     _id: 'q2',
     title: '问卷2',
     isPublished: true,
-    isStar: false,
+    isStar: true,
     answerCount: 5,
     createdAt: '3月10日 13:23',
   },
@@ -25,7 +25,7 @@ const rawQuestionList = [
     _id: 'q3',
     title: '问卷3',
     isPublished: false,
-    isStar: false,
+    isStar: true,
     answerCount: 5,
     createdAt: '3月10日 13:23',
   },
@@ -57,12 +57,14 @@ const List: FC = () => {
         <div className={Styles.right}>(搜索)</div>
       </div>
       <div className={Styles.content}>
-        {questionList.map(i => {
-          const { _id } = i
-          return <QuestionCard key={_id} {...i}></QuestionCard>
-        })}
+        {/* 问卷列表 */}
+        {questionList.length > 0 &&
+          questionList.map(i => {
+            const { _id } = i
+            return <QuestionCard key={_id} {...i}></QuestionCard>
+          })}
       </div>
-      <div className={Styles.footer}>footer</div>
+      <div className={Styles.footer}>loadMore... 上划加载更多...</div>
     </>
   )
 }
