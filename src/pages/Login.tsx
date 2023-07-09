30b567e5f5eb79b1292663efebed44a213a8c541
凉风有信 <3042146237@qq.com>
2023-07-09 14:36:29 +0800

    feat: 登录页和注册页UI和基础功能

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,102 @@
-import React, { FC } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { FC, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Styles from './Login.module.scss'
+import { Typography, Space, Form, Input, Button, Checkbox } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import { REGISTER_PATHNAME } from '../router'
+
+type FormType = {
+  username: string
+  password: string
+  remember: boolean
+}
+
+const USERNAME_KEY = 'username'
+const PASSWORD_KEY = 'password'
+
+function rememberUser(username: string, password: string) {
+  localStorage.setItem(USERNAME_KEY, username)
+  localStorage.setItem(PASSWORD_KEY, password)
+}
+function deleteUserFormStorage() {
+  localStorage.removeItem(USERNAME_KEY)
+  localStorage.removeItem(PASSWORD_KEY)
+}
+function getUserInfoFormStorage() {
+  return {
+    username: localStorage.getItem(USERNAME_KEY),
+    password: localStorage.getItem(PASSWORD_KEY),
+  }
+}
+
+const { Title } = Typography
 
 const Login: FC = () => {
-  const nav = useNavigate()
+  const [form] = Form.useForm()
+  useEffect(() => {
+    const { username, password } = getUserInfoFormStorage()
+    form.setFieldsValue({ username, password })
+  }, [])
+
+  const onFinish = (values: FormType) => {
+    if (values.remember) {
+      rememberUser(values.username, values.password)
+    } else {
+      deleteUserFormStorage()
+    }
+  }
 
   return (
-    <div>
-      <p>Login</p>
-      <button onClick={() => nav(-1)}>返回上一个路由</button>
+    <div className={Styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>用户登陆</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          autoComplete="true"
+          form={form}
+        >
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              { required: true, message: '请输入你的用户名！' },
+              { type: 'string', min: 5, max: 20, message: '字符长度在5到20之间' },
+              { pattern: /^\w+$/, message: '只能是字母数字下划线' },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: '请输入你的密码！' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 6, span: 16 }}>
+            <Checkbox>记住我</Checkbox>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                登陆
+              </Button>
+              <Link to={REGISTER_PATHNAME}>去注册新用户</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   )
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,95 @@
 import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
+import Styles from './Register.module.scss'
+import { Typography, Space, Form, Input, Button } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import { LOGIN_PATHNAME } from '../router'
+
+type RegisterFormType = {
+  username: string
+  password: string
+  confirm: string
+  nickname: string
+}
+
+const { Title } = Typography
 
 const Register: FC = () => {
-  return <p>Register</p>
+  const onFinish = (values: RegisterFormType) => {
+    console.log(values)
+  }
+  return (
+    <div className={Styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>注册新用户</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              { required: true, message: '请输入你的用户名！' },
+              { type: 'string', min: 5, max: 20, message: '字符长度在5到20之间' },
+              { pattern: /^\w+$/, message: '只能是字母数字下划线' },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: '请输入你的密码！' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="确认密码"
+            name="confirm"
+            // 依赖 password，password 变化会重新触发 validator 验证
+            dependencies={['password']}
+            rules={[
+              { required: true, message: '请确认你的密码！' },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) return Promise.resolve()
+                  else return Promise.reject(new Error('两次密码不一致！'))
+                },
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="昵称"
+            name="nickname"
+            rules={[{ required: true, message: '请输入你的昵称！' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                注册
+              </Button>
+              <Link to={LOGIN_PATHNAME}>有账号？去登陆</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
 }
 
 export default Register
