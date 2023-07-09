70faf01389c6ead6c462f3fab1d50626528064b5
凉风有信 <3042146237@qq.com>
2023-07-09 23:05:53 +0800

    feat: 为我的问卷页添加下滑加载更多

--- src/pages/manage/List/index.tsx
+++ src/pages/manage/List/index.tsx
@@ -1,24 +1,84 @@
-import React, { FC } from 'react'
-// import { useSearchParams } from 'react-router-dom'
-import { useTitle } from 'ahooks'
+import React, { FC, useEffect, useState, useRef } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { useTitle, useDebounceFn, useRequest } from 'ahooks'
 import Styles from '../Common/Common.module.scss'
-import { Typography, Spin } from 'antd'
+import { Typography, Spin, Empty } from 'antd'
 import QuestionCard from '../../../components/QuestionCard'
 import ListSearch from '../../../components/ListSearch'
-import useLoadQuestionList from '../../../hooks/useLoadQuestionList'
+import { getQuestionListApi } from '../../../services/question'
+import { LIST_PAGE_SIZE, LIST_SEARCH_PARAM_NAME } from '../../../constant'
 
 const { Title } = Typography
 
 const List: FC = () => {
   useTitle('我的问卷！')
 
+  const [page, setPage] = useState(1)
+  const [questionList, setQuestionList] = useState([])
+  const [total, setTotal] = useState(0)
+  const haveMoreData = total > questionList.length
+
+  const [searchParams] = useSearchParams()
+
+  // * 真正加载：
+  const { run: load, loading } = useRequest(
+    async () => {
+      const data = await getQuestionListApi({
+        page,
+        pageSize: LIST_PAGE_SIZE,
+        keyword: searchParams.get(LIST_SEARCH_PARAM_NAME) || '',
+      })
+
+      return data
+    },
+    {
+      manual: true,
+      onSuccess({ list = [], total = 0 }) {
+        // 累计：
+        setQuestionList(list.concat(list))
+        setTotal(total)
+        setPage(page + 1)
+      },
+    }
+  )
+
+  const containerRef = useRef<HTMLDivElement>(null)
+  // 添加防抖：
+  const { run: tryLoadMore } = useDebounceFn(
+    () => {
+      const elem = containerRef.current
+      if (elem == null) return
+      const domRect = elem.getBoundingClientRect()
+      if (domRect == null) return
+      const { bottom } = domRect
+      if (bottom <= document.body.clientHeight) {
+        load()
+      }
+    },
+    { wait: 500 }
+  )
+
+  // 1、页面首次加载，或者 url 参数（keyword）变化时，触发加载。。。
+  useEffect(() => {
+    tryLoadMore()
+  }, [searchParams])
+  // 2、当页面滚动时，触发加载。。。
+  useEffect(() => {
+    if (haveMoreData) {
+      window.addEventListener('scroll', tryLoadMore)
+    }
+    return () => {
+      window.removeEventListener('scroll', tryLoadMore)
+    }
+  }, [searchParams, haveMoreData])
+
   // const [searchParams] = useSearchParams()
   // console.log('keyword', searchParams.get('keyword'))
   // console.log('age', searchParams.get('age'))
 
   // * 版本三： 使用 useLoadQuestionList 自定义钩子的写法：
-  const { data = {}, loading } = useLoadQuestionList()
-  const { list: questionList = [] } = data
+  // const { data = {}, loading } = useLoadQuestionList()
+  // const { list: questionList = [] } = data
 
   // *版本二： 使用 useRequest 这个 hook 的写法：
   // const { data = {}, loading } = useRequest(getQuestionListApi)
@@ -40,6 +100,12 @@ const List: FC = () => {
   //   getQuestionList()
   // }, [])
 
+  const LoadMoreContentElem = () => {
+    if (!loading) return <Spin />
+    if (total == 0) return <Empty description="暂无数据" />
+    if (!haveMoreData) return <span>没有更多了...</span>
+    return <span>开始加载下一页...</span>
+  }
   return (
     <>
       <div className={Styles.header}>
@@ -51,21 +117,16 @@ const List: FC = () => {
         </div>
       </div>
       <div className={Styles.content}>
-        {/* 是loading 是加载 loading 动画 */}
-        {loading && (
-          <div style={{ textAlign: 'center' }}>
-            <Spin size="large" />
-          </div>
-        )}
-        {/* 不是 loading 时并且问卷列表长度大于 0 时，加载 问卷列表 */}
-        {!loading &&
-          questionList.length > 0 &&
+        {/* 问卷列表 */}
+        {questionList.length > 0 &&
           questionList.map((i: any) => {
             const { _id } = i
             return <QuestionCard key={_id} {...i}></QuestionCard>
           })}
       </div>
-      <div className={Styles.footer}>loadMore... 上划加载更多...</div>
+      <div className={Styles.footer}>
+        <div ref={containerRef}>{LoadMoreContentElem()}</div>
+      </div>
     </>
   )
 }
